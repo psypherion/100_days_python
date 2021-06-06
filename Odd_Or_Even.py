08e49aa518714473a185d7642fdd8330516d0210
kyle williams <72155617+ky13-troj@users.noreply.github.com>
2021-06-07 00:05:28 +0530

    Create Odd_Or_Even.py
    A program to Check if A number if it's even or odd

---
+++ Odd_Or_Even.py
@@ -0,0 +1,9 @@
+# exercise -1
+# ODD OR EVEN
+num = int(input("Enter your number:"))
+if num == 0:
+  print("0 is neither even nor odd")
+elif num%2 == 0:
+  print("Your number is even")
+else :
+  print("Your number is odd")
