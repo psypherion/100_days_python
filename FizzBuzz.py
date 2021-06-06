5d932638bfc27b2f997820cfd06c1e14254fcb16
kyle williams <72155617+ky13-troj@users.noreply.github.com>
2021-06-07 00:22:25 +0530

    Create FizzBuzz.py
    Fizz Buzz Game

---
+++ FizzBuzz.py
@@ -0,0 +1,11 @@
+# exercise-4
+# FizzBuzz Problem:
+for i in range(1,101):
+  if i%15 == 0:
+    print("FizzBuzz")
+  elif i%3 == 0:
+    print("Fizz")
+  elif i%5 == 0 :
+    print("Buzz")
+  else :
+    print(i)
